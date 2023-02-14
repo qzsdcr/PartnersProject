6236d69359011828184f2bcdde01994c80223fe7
qzsdcr <widm5fjwl@naver.com>
2023-02-14 10:19:18 +0900

    idao

--- src/main/java/com/tech/sprj09/dao/IDao.java
+++ src/main/java/com/tech/sprj09/dao/IDao.java
@@ -8,37 +8,33 @@ import com.tech.sprj09.dto.SikdangDto;
 
 public interface IDao {
 
-	// 기본 리스트 ,디테일, 수정 ,삭제 ,리플 , 카테고리별 검색, 하위 리플 삭제 방지 !!시작!!
-	public ArrayList<BoardDto> list(String sk, String selNum);
-
-	public void write(String bname, String btitle, String bcontent, String fname);
-
-	public BoardDto contentview(String gbid);
-
-	public void upHit(String gbid);
-
-	public BoardDto contentupdate(String gbid);
-
-	public void modify(String bid, String bname, String btitle, String bcontent);
-
-	public void delete(String bid);
-
-	public BoardDto replyview(String gbid);
-
-	public void reply(String bid, String bname, String btitle, String bcontent, String bgroup, String bstep,
-			String bindent);
-
-	public int replyShape(String bgroup, String bstep);
-
-	public int selectBoardTotCount1(String searchKeyword);
-
-	public int selectBoardTotCount2(String searchKeyword);
-
-	public int selectBoardTotCount3(String searchKeyword);
-
-	public int selectBoardTotCount4(String searchKeyword);
-
-	public ArrayList<BoardDto> sonReply(int bgroup);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//회원가입 DBinsert
 	public void join(String memid, String mempass, String memname, String memsex, String membirth, String mememail,
