2ac0b3b379eff0c023889b3f202b080892a4f23e
qzsdcr <widm5fjwl@naver.com>
2023-03-03 11:40:02 +0900

    list add plus

--- src/main/java/com/tech/sprj09/HomeController.java
+++ src/main/java/com/tech/sprj09/HomeController.java
@@ -44,7 +44,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		model.addAttribute("serverTime", formattedDate );
 	    
-	    return "home";
+	    return "redirect:list";
 	}
 	
 	@RequestMapping(value = "/chat", method = RequestMethod.GET)
